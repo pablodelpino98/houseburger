bc08641a09cff13f73ad00686739e3f17b0d0769
pablodelpino98 <pablo19delpino98@gmail.com>
2025-05-17 15:46:25 +0100

    Added language files

---
+++ lang/es.php
@@ -0,0 +1,8 @@
+<?php
+$translations = [
+    'title' => 'Burger House',
+    'login' => 'Iniciar Sesión',
+    'logout' => 'Cerrar Sesión',
+    'cart_empty' => 'Inicie sesión para usar el carrito',
+    // Agrega más traducciones necesarias aquí
+];
